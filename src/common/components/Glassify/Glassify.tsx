deddc452f28b9c3e47e24a5878a0c190d4d4f573
M. Rasyid Ridho <rasyid.rdh05@gmail.com>
2023-10-07 12:17:45 +0700

    fix image mines

--- src/common/components/Glassify/Glassify.tsx
+++ src/common/components/Glassify/Glassify.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Glassify = () => {
   return (
     <>
-       <img alt="" src="/images/glassify/blur-cyan-0.png" width="530" height="530" decoding="async" data-nimg="1" className="fixed right-full bottom-0 -mr-72 -mb-56 opacity-50" />
+       <Image alt="" src="/images/glassify/blur-cyan-0.png" width="530" height="530" decoding="async" data-nimg="1" className="fixed right-full bottom-0 -mr-72 -mb-56 opacity-50" />
     </>
   )
 }
