9f61ad540be1260dff471300fd830af1ac6a2147
M. Rasyid Ridho <rasyid.rdh05@gmail.com>
2024-02-29 10:48:08 +0700

    Add Dribble

--- src/common/widgets/Socials/config/constans.tsx
+++ src/common/widgets/Socials/config/constans.tsx
@@ -1,10 +1,10 @@
-import { FiGithub, FiLinkedin,  FiInstagram   } from 'react-icons/fi'
-import { SocialTypes } from './types'
-
+import { FiGithub, FiLinkedin, FiInstagram, FiDribbble } from "react-icons/fi";
+import { SocialTypes } from "./types";
 
 // social media data
-export const socialData : SocialTypes[] = [
-    { icon : FiGithub, url : "https://github.com/rasyidridho05" },
-    { icon : FiLinkedin, url : "https://www.linkedin.com/in/rasyidrdh05/" },
-    { icon : FiInstagram, url : "https://www.instagram.com/rasyidrdh05/" },
-]
+export const socialData: SocialTypes[] = [
+  { icon: FiGithub, url: "https://github.com/rasyidridho05" },
+  { icon: FiLinkedin, url: "https://www.linkedin.com/in/rasyidrdh05/" },
+  { icon: FiInstagram, url: "https://www.instagram.com/rasyidrdh05/" },
+  { icon: FiDribbble, url: "https://dribbble.com/rasyidridho05" },
+];
