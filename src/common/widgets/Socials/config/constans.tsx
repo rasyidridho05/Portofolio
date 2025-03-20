f86c8b24f934060c279e49c63eb971e53be0b295
M. Rasyid Ridho <rasyid.rdh05@gmail.com>
2025-03-20 11:39:07 +0700

    update link

--- src/common/widgets/Socials/config/constans.tsx
+++ src/common/widgets/Socials/config/constans.tsx
@@ -4,7 +4,7 @@ import { SocialTypes } from "./types";
 // social media data
 export const socialData: SocialTypes[] = [
   { icon: FiGithub, url: "https://github.com/rasyidridho05" },
-  { icon: FiLinkedin, url: "https://www.linkedin.com/in/rasyidrdh05/" },
+  { icon: FiLinkedin, url: "https://www.linkedin.com/in/rasyidridho05/" },
   { icon: FiInstagram, url: "https://www.instagram.com/rasyidrdh05/" },
   { icon: FiDribbble, url: "https://dribbble.com/rasyidridho05" },
 ];
